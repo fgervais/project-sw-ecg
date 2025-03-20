75f90c28f2732e42e992564ceeae4c850877db91
Francois Gervais <francoisgervais@gmail.com>
2025-03-19 21:04:57 -0400

    Remove old code

--- main.py
+++ main.py
@@ -6,54 +6,12 @@ import time
 from server import MyServer
 
 
-# server = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
-# # server.bind(("::1", 50000))
-# server.bind(("fd04:2240::1cef", 50000))
-# server.listen()
-
-# (conn, address) = server.accept()
-
-# while True:
-#     time.sleep(1)
-
-
-# def signal_handler(sig, frame):
-#     print("You pressed Ctrl+C!")
-#     server.shutdown()
-
-
-
 # Used by docker-compose down
 def sigterm_handler(signal, frame):
     global teardown
 
     logger.info("💥 Reacting to SIGTERM")
     teardown = True
-
-
-
-
-
-
-        # print(self.data.decode("utf-8"))
-        # just send back the same data, but upper-cased
-        # self.request.sendall(self.data.upper())
-        # after we return, the socket will be closed.
-
-
-# signal.signal(signal.SIGINT, signal_handler)
-
-# HOST, PORT = "fd04:2240::1cef", 50000
-
-
-# server = TCPServer6((HOST, PORT), MyTCPHandler)
-# try:
-#     server.serve_forever()
-# except KeyboardInterrupt:
-#     pass
-
-# server.server_close()
-
 
 
 logging.basicConfig(
